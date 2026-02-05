5f38b234e677f88a7fe205b6e4d11fcb37c737b0
Samuel <samuelop3308@gmail.com>
2026-02-04 21:34:11 -0500

    más interfaz

--- CrepesAndWaffles/src/co/edu/unbosque/view/Seleccion.java
+++ CrepesAndWaffles/src/co/edu/unbosque/view/Seleccion.java
@@ -27,6 +27,10 @@ public class Seleccion extends JFrame{
 	private JLabel lblHelado;
 	private JLabel lblWaffle;
 	private JLabel lblCrepe;
+	private JButton btnVolver;
+	private ImageIcon imgCarrito;
+	private JButton btnCarrito;
+	private JLabel lblCarrito;
 	
 	public Seleccion() {
 		asignarComponentes();
@@ -41,7 +45,7 @@ public class Seleccion extends JFrame{
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		this.setLayout(null);
-		this.setBackground(new Color(201, 148, 117));
+		this.setBackground(Color.white);
 		this.setPreferredSize(new Dimension(1200, 675));
 		this.pack();
 		this.setLocationRelativeTo(null);
@@ -53,33 +57,18 @@ public class Seleccion extends JFrame{
 		lblOpcion.setBounds(225, 50, 1200, 70);
 		this.add(lblOpcion);
 		
-		
-		imgWaffle = new ImageIcon("src/co/edu/unbosque/view/waffle.jpg");
-		imagen = imgWaffle.getImage();
-		escalada = imagen.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
-		etiqueta = new JLabel(new ImageIcon(escalada));
-		add(etiqueta);
+
 		
 		btnWaffle = new JButton(new ImageIcon("src/co/edu/unbosque/view/waffle.jpg"));
 		btnWaffle.setBounds(100, 150, 300, 400);
 		this.add(btnWaffle);
 		
-		imgHelado = new ImageIcon("src/co/edu/unbosque/view/helado.jpg");
-		imagen = imgHelado.getImage();
-		escalada = imagen.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
-		etiqueta = new JLabel(new ImageIcon(escalada));
-		add(etiqueta);
+
 		
 		btnHelado = new JButton(new ImageIcon("src/co/edu/unbosque/view/helado.jpg"));
 		btnHelado.setBounds(450, 150, 300, 400);
 		this.add(btnHelado);
 		
-		
-		imgCrepe = new ImageIcon("src/co/edu/unbosque/view/crepe.jpg");
-		imagen = imgCrepe.getImage();
-		escalada = imagen.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
-		etiqueta = new JLabel(new ImageIcon(escalada));
-		add(etiqueta);
 		
 		
 		btnCrepe = new JButton(new ImageIcon("src/co/edu/unbosque/view/crepe.jpg"));
@@ -103,7 +92,197 @@ public class Seleccion extends JFrame{
 		lblCrepe.setBounds(875, 550, 200, 50);
 		this.add(lblCrepe);
 		
+		imgCarrito = new ImageIcon("src/co/edu/unbosque/view/carritoCompras.png");
+		imagen = imgCarrito.getImage();
+		escalada = imagen.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+		etiqueta = new JLabel(new ImageIcon(escalada));
+		add(etiqueta);
 		
+		btnCarrito = new JButton(new ImageIcon(escalada));
+		btnCarrito.setBounds(1050, 20, 75, 75);
+		this.add(btnCarrito);
+		
+
+		
+		btnVolver = new JButton("Volver");
+		btnVolver.setFont(new Font("Arial Rounded MT Bold", Font.BOLD,30));
+		btnVolver.setBounds(10, 20, 150, 50);
+		btnVolver.setBackground(Color.white);
+		btnVolver.setForeground(Color.BLACK);
+		btnVolver.setFocusPainted(false);
+		btnVolver.setBorder(null);
+		this.add(btnVolver);
+		
+	}
+
+
+	public JLabel getLblOpcion() {
+		return lblOpcion;
+	}
+
+
+	public void setLblOpcion(JLabel lblOpcion) {
+		this.lblOpcion = lblOpcion;
+	}
+
+
+	public ImageIcon getImgWaffle() {
+		return imgWaffle;
+	}
+
+
+	public void setImgWaffle(ImageIcon imgWaffle) {
+		this.imgWaffle = imgWaffle;
+	}
+
+
+	public ImageIcon getImgHelado() {
+		return imgHelado;
+	}
+
+
+	public void setImgHelado(ImageIcon imgHelado) {
+		this.imgHelado = imgHelado;
+	}
+
+
+	public ImageIcon getImgCrepe() {
+		return imgCrepe;
+	}
+
+
+	public void setImgCrepe(ImageIcon imgCrepe) {
+		this.imgCrepe = imgCrepe;
+	}
+
+
+	public JLabel getEtiqueta() {
+		return etiqueta;
+	}
+
+
+	public void setEtiqueta(JLabel etiqueta) {
+		this.etiqueta = etiqueta;
+	}
+
+
+	public Image getImagen() {
+		return imagen;
+	}
+
+
+	public void setImagen(Image imagen) {
+		this.imagen = imagen;
+	}
+
+
+	public Image getEscalada() {
+		return escalada;
+	}
+
+
+	public void setEscalada(Image escalada) {
+		this.escalada = escalada;
+	}
+
+
+	public JButton getBtnWaffle() {
+		return btnWaffle;
+	}
+
+
+	public void setBtnWaffle(JButton btnWaffle) {
+		this.btnWaffle = btnWaffle;
+	}
+
+
+	public JButton getBtnCrepe() {
+		return btnCrepe;
+	}
+
+
+	public void setBtnCrepe(JButton btnCrepe) {
+		this.btnCrepe = btnCrepe;
+	}
+
+
+	public JButton getBtnHelado() {
+		return btnHelado;
+	}
+
+
+	public void setBtnHelado(JButton btnHelado) {
+		this.btnHelado = btnHelado;
+	}
+
+
+	public JLabel getLblHelado() {
+		return lblHelado;
+	}
+
+
+	public void setLblHelado(JLabel lblHelado) {
+		this.lblHelado = lblHelado;
+	}
+
+
+	public JLabel getLblWaffle() {
+		return lblWaffle;
+	}
+
+
+	public void setLblWaffle(JLabel lblWaffle) {
+		this.lblWaffle = lblWaffle;
+	}
+
+
+	public JLabel getLblCrepe() {
+		return lblCrepe;
+	}
+
+
+	public void setLblCrepe(JLabel lblCrepe) {
+		this.lblCrepe = lblCrepe;
+	}
+
+
+	public JButton getBtnVolver() {
+		return btnVolver;
+	}
+
+
+	public void setBtnVolver(JButton btnVolver) {
+		this.btnVolver = btnVolver;
+	}
+
+
+	public ImageIcon getImgCarrito() {
+		return imgCarrito;
+	}
+
+
+	public void setImgCarrito(ImageIcon imgCarrito) {
+		this.imgCarrito = imgCarrito;
+	}
+
+
+	public JButton getBtnCarrito() {
+		return btnCarrito;
+	}
+
+
+	public void setBtnCarrito(JButton btnCarrito) {
+		this.btnCarrito = btnCarrito;
+	}
+
+
+	public JLabel getLblCarrito() {
+		return lblCarrito;
+	}
+
+
+	public void setLblCarrito(JLabel lblCarrito) {
+		this.lblCarrito = lblCarrito;
 	}
 	
 	
